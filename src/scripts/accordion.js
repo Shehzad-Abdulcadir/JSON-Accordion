4e30e839ec3873a968d053bda398f68f8ef6d92a
Shehzad Abdulcadir <Shehzad-Abdulcadir@users.noreply.github.com>
2017-10-08 00:19:25 +0100

    Code Cleanup - Add Comments, Rename objects/methods to more appropriate names

--- src/scripts/accordion.js
+++ src/scripts/accordion.js
@@ -1,11 +1,13 @@
 jQuery(document).ready(function($){
 
+// Create accordion object
 var accordion = {
     init: function() {
         this.accordionJson = 'http://design.propcom.co.uk/buildtest/accordion-data.json';
         this.getData();
     },
 
+    // Pull in JSON Data using AJAX Request
     getData: function() {
         $.ajax({
             url: accordion.accordionJson,
@@ -18,6 +20,7 @@ var accordion = {
         });
     },
 
+    // Create wrapper for each accordion
     iterateAccordionData: function(accordionData) {
         for ( i = 0; i < accordionData.length; i++) {
             $( ".accordion" ).append( "<article>" );
@@ -26,6 +29,7 @@ var accordion = {
         accordion.appendAccordionContent(accordionData);
     },
 
+    // Append JSON Data to relevant tags
     appendAccordionContent: function(accordionData) {
         $('article').each(function(index) {
             $('article').eq(index).append('<h2>'+accordionData[index]['heading']+'</h2>');
@@ -34,11 +38,13 @@ var accordion = {
         });
     },
 
+    // Hide answer and run toggle function
     hideAccordion: function() {
         $('p').hide();
         accordion.toggleAccordion();
     },
 
+    // Show/Hide answer + add active class on wrapper
     toggleAccordion: function() {
         $('article').on('click', function() {
             $(this).find('p').slideToggle();
@@ -46,8 +52,8 @@ var accordion = {
         });
     },
 }
+//Init Accordion Oject
 accordion.init();
-
 });
 
 
